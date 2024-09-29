091d9c843bcfd4ed068e13d1714572e0140fa530
ktunchar <maikittitee1615@gmail.com>
2024-09-29 21:46:25 +0700

    feat: card component

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const ProfileCard = () => {
-  return (
-	<div>
-
-	</div>
-  )
+interface ProfileCardProps {
+  name: string;
+  image: string;
+  category: string;
+  year: string;
+  birthdate: string;
+  birth_place: string;
+  quote: string;
 }
 
-export default ProfileCard
+const ProfileCard: React.FC<ProfileCardProps> = ({
+  name,
+  image,
+  category,
+  year,
+  birthdate,
+  birth_place,
+  quote,
+}) => {
+  return (
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
+      <img className="w-full h-64 object-cover" src={image} alt={name} />
+      <div className="p-6">
+        <h2 className="outfit text-gray-700 font-extrabold text-xl mb-2">
+          {name}
+        </h2>
+        <p className="text-gray-700 text-base">
+          <strong>Category:</strong> {category}
+        </p>
+        <p className="outfit text-gray-700 text-base">
+          <strong>Year:</strong> {year}
+        </p>
+        <p className="outfit text-gray-700 text-base">
+          <strong>Birthdate:</strong> {birthdate}
+        </p>
+        <p className="outfit text-gray-700 text-base">
+          <strong>Birthplace:</strong> {birth_place}
+        </p>
+        <p className="kanit-thin text-gray-700 text-base italic mt-4">
+          “{quote}”
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
     extend: {
       colors: {
         background: "var(--background)",
