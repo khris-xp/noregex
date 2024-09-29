bac57153e74bb6e0ff7adf3e42d8df3f5506a57e
khris-xp <khrisbharmmano@gmail.com>
2024-09-29 10:17:59 +0700

    feat(layout): sidebar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import SidebarLayout from "@/layouts/sidebar.layout";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarLayout>{children}</SidebarLayout>
       </body>
     </html>
   );
