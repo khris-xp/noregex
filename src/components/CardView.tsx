610e580dd6b07ccc8314fb55950e2dcf1d802f32
khris-xp <khrisbharmmano@gmail.com>
2024-10-05 09:59:18 +0700

    refac: refactor sidebar handle state with change header

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -10,7 +10,7 @@ const CardView: React.FC<Props> = ({ data }) => {
   return (
     <div className="rounded-2xl w-full h-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6">
       {data.map((ele, index) => (
-        <div className="flex justify-center">
+        <div className="flex justify-center" key={index}>
           <ProfileCard key={index} data={ele} />
         </div>
       ))}
